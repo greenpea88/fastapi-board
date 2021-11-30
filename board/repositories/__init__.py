f8431a5ef5da532a6a100591afa97a0e12359af3
greenpea88 <green_pea88@naver.com>
2021-11-30 10:38:03 +0900

    :memo: UPDATE: refactoring을 위해 설정해두었던 repository init 주석 처리

--- board/repositories/__init__.py
+++ board/repositories/__init__.py
@@ -30,37 +30,37 @@ class MakeSession:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.session.close()
 
-class BaseRepo():
-    model: Base = NotImplemented
-
-    def __init__(self):
-        db_user = 'greenpea'
-        db_password = ''
-        db_host = 'localhost'
-        db_port = '5432'
-        db_database = 'board'
-
-        dsn = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
-        self.engine = create_engine(dsn, echo=True)
-        Base.metadata.create_all(self.engine)
-
-    @property
-    def makeSession(self):
-
-        @contextmanager
-        def session():
-            Session = sessionmaker(bind=self.engine)
-            local_session = Session()
-            try:
-                yield local_session
-            except:
-                local_session.rollback()
-                raise
-            finally:
-                local_session.close()
-
-        return session
-
-    def get(self, query, user_id):
-        with self.makeSession as session:
-            query - session.query(self.model)
+# class BaseRepo():
+#     model: Base = NotImplemented
+#
+#     def __init__(self):
+#         db_user = 'greenpea'
+#         db_password = ''
+#         db_host = 'localhost'
+#         db_port = '5432'
+#         db_database = 'board'
+#
+#         dsn = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
+#         self.engine = create_engine(dsn, echo=True)
+#         Base.metadata.create_all(self.engine)
+#
+#     @property
+#     def makeSession(self):
+#
+#         @contextmanager
+#         def session():
+#             Session = sessionmaker(bind=self.engine)
+#             local_session = Session()
+#             try:
+#                 yield local_session
+#             except:
+#                 local_session.rollback()
+#                 raise
+#             finally:
+#                 local_session.close()
+#
+#         return session
+#
+#     def get(self, query, user_id):
+#         with self.makeSession as session:
+#             query - session.query(self.model)
